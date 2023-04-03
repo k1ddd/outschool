92baf131075a0d302eeb5dbc0d2140bc74f72337
vladyslav <vladyslav.klokun@gmail.com>
2023-04-03 21:54:11 +0300

    add new project and working in

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      lg: { max: "1999.99px" },
+      md: { max: "991.99px" },
+      sm: { max: "767.99px" },
+      xs: { max: "479.99px" },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        heroGradient:
+          "linear-gradient(94.59deg, #4923B4 2.39%, #E878CF 97.66%);",
+      },
+      colors: {
+        blueviolet: "#5027B5",
+        lightgray: "#666768",
+      },
+    },
+  },
+  plugins: [],
+};
